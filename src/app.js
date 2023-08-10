5d9e78ded3f42140204107761ddb05001cae561f
parvinzaman <fuksit90@gmail.com>
2023-08-10 20:33:58 +0330

    showing current temp

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function displaytemp(response) {
+  let tempratureElement = document.querySelector("#temprature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  tempratureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "082d3d02ffdb12f2fd9b259e2ced1d0d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric `;
+
+axios.get(apiUrl).then(displaytemp);
